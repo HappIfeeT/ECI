14513c2aaa6126464a97d30aeb6b222d98124ae1
HappIfeeT <reinert.fabrice@orange.fr>
2015-10-28 23:09:20 +0100

    Update ECI_functions.hpp

--- ECI.Altis/ECI_functions.hpp
+++ ECI.Altis/ECI_functions.hpp
@@ -12,17 +12,5 @@ class ECI
 	{
 		class announceLevelBegin {};
 		class announceLevelEnd {};
-		class announceRoundBegin {};
-		class announceRoundEnd {};
-	};
-	class aimngt
-	{
-		class setSkill {description = "Define unit skill array";};
-		class housesOccupation {description = "Militarize nearest houses";};
-		class vehiclePatrol {description = "Spawn vehicle and move along waypoints";};
-	};
-	class arrays
-	{
-		class arrayInvert {description = "Inverts indexes of an array";};
 	};
 };
